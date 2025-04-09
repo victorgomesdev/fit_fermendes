4123be5e774eef7c3b4822bb475ea524c56b1aa8
Victor <victorgomesnog123@gmail.com>
2025-04-09 17:48:44 -0300

    chore: Mudança nas pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app.routing.module";
 import { BrowserModule } from "@angular/platform-browser";
-import { SharedModule } from "./app/shared/shared.module";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
     declarations: [AppComponent],
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
 platformBrowserDynamic().bootstrapModule(AppModule)
