cde10d7391114df905e102f5d83ce1fac1bd3484
Victor <victorgomesnog123@gmail.com>
2025-04-10 15:17:26 -0300

    chore: Adiciona o pacote primeicons e adiciona novas cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,17 @@ module.exports = {
   theme: {
 
     colors: {
+      'white': '#ffffff',
       'primary': '#ff69b4',
       'secondary': '#c9639b',
       'tertiary': 'rgba(255, 105, 180, 0.1)',
       'error': '#e74c3c',
       'success': '#2ecc71',
-      'background': '#f0f2f5'
+      'success-bg': 'rgba(46, 204, 113, 0.1)',
+      'background': '#f0f2f5',
+      'info': "#3498db",
+      'yellow': '#f3bb1f',
+      'orange': 'rgba(255, 255, 255, 0)'
     },
     fontFamily:{
       
