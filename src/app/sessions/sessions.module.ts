95a51408df9cf5688c90cc75e44136052511849c
Victor Gomes <104176261+victorgomesdev@users.noreply.github.com>
2025-04-11 00:42:47 +0000

    chore: Alterações em configurações de importação

--- src/app/sessions/sessions.module.ts
+++ src/app/sessions/sessions.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from "@angular/core";
 import { SessionsRoutingModule } from "./sessions.routing.module";
-import { SharedModule } from "../shared/shared.module";
+import { SharedModule } from "@shared/shared.module";
 import { SessionCardComponet } from "./session-card/session-card.component";
 import { SummaryComponent } from "./summary/summary.component";
+import { ScheduleComponent } from "./schedule/schedule.component";
+import { CalendarModule, DateAdapter } from 'angular-calendar'
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
-    imports: [SessionsRoutingModule, SharedModule],
+    imports: [
+        SessionsRoutingModule,
+        SharedModule,
+        CalendarModule.forRoot({
+            provide: DateAdapter,
+            useFactory: adapterFactory
+        })
+    ],
     declarations: [
         SessionCardComponet,
-        SummaryComponent
+        SummaryComponent,
+        ScheduleComponent
     ]
 })
-export class SessionsModule {}
+export class SessionsModule { }
