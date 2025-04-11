725746930c81b4149571192e2dd04b65b866066f
Victor <victorgomesnog123@gmail.com>
2025-04-11 15:12:36 -0300

    chore: Adiciona novas cores e padrões de estilização globais

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,26 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
+    extend: {
+      colors: {
+        'primary': '#ff69b4',
+        'secondary': '#c9639b',
+        'tertiary': 'rgba(255, 105, 180, 0.1)',
+        'error': '#e74c3c',
+        'success': '#2ecc71',
+        'success-bg': 'rgba(46, 204, 113, 0.1)',
+        'background': '#f0f2f5',
+        'info': "#3498db",
+        'yellow': '#f3bb1f',
+        'orange': 'rgba(255, 255, 255, 0)',
+        'paragraph': '#444'
+      },
+      fontFamily: {
 
-    colors: {
-      'white': '#ffffff',
-      'primary': '#ff69b4',
-      'secondary': '#c9639b',
-      'tertiary': 'rgba(255, 105, 180, 0.1)',
-      'error': '#e74c3c',
-      'success': '#2ecc71',
-      'success-bg': 'rgba(46, 204, 113, 0.1)',
-      'background': '#f0f2f5',
-      'info': "#3498db",
-      'yellow': '#f3bb1f',
-      'orange': 'rgba(255, 255, 255, 0)'
-    },
-    fontFamily:{
-      
+      },
+      boxShadow: {
+        "input-focus": "0 0 5px #ff69b4"
+      }
     }
 
   },
