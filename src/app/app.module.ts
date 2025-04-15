722a28286286da632b74bbf3192eca79b721011b
Victor Gomes <104176261+victorgomesdev@users.noreply.github.com>
2025-04-15 00:30:12 +0000

    feat: Adiciona o módulo de animações em AppModule e configura/estiliza o calendário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app.routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from "@angular/platform-browser";
 import { SharedModule } from "@shared/shared.module";
 import { SessionsModule } from "@sessions/sessions.module";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app.routing.module";
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         AppRoutingModule,
         BrowserModule,
+        BrowserAnimationsModule,
         SharedModule,
         SessionsModule
     ],
--- src/app/sessions/schedule/schedule.component.ts
+++ src/app/sessions/schedule/schedule.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { CalendarEvent } from 'angular-calendar'
-import { Locale } from 'date-fns/locale'
+import { addDays } from 'date-fns'
 
 @Component({
     templateUrl: './schedule.component.html',
@@ -10,18 +10,47 @@ import { Locale } from 'date-fns/locale'
 export class ScheduleComponent {
 
     today!: Date
+    targetDate!: Date
+
     sessions!: CalendarEvent[]
-    calendarLocale!: Locale
+
+    showActiveDay = false
     showModal: boolean = false
-    
+
     constructor() {
         this.today = new Date()
         this.sessions = [
             {
                 title: "Aula 1",
                 start: this.today,
+                
+            },
+            {
+                title: "Aula 2",
+                start: addDays(this.today, 3),
+            },
+            {
+                title: "Aula 3",
+                start: addDays(this.today, 4),
+            },
+            {
+                title: "Aula 4",
+                start: addDays(this.today, 6),
+            },
+            {
+                title: "Aula 5",
+                start: addDays(this.today, 10),
             }
         ]
+    }
+
+    dayClicked(day: any) {
+        this.targetDate = day.date
+        this.showActiveDay = this.showActiveDay ? false : (this.hasEventInThisDate(day.date) ? true : false)
+    }
+
+    private hasEventInThisDate(target: Date) {
+        return this.sessions.some(s => s.start.getDate() === target.getDate())
     }
 
     toggleModal(): void {
