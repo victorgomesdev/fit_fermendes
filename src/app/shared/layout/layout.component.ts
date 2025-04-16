785afbe5b0cd04795a7f776dcf8f1cdb202fcad6
Victor <victorgomesnog123@gmail.com>
2025-04-16 15:27:32 -0300

    fix: Conserta a cor do link ativo na navbar

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -2,6 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     templateUrl: './layout.component.html',
+    styles: `
+        .active {
+            background-color: #ffffff;
+            color: #c9639b
+        }
+    `,
     selector: 'layout',
     standalone: false
 })
